a8c0d4321b9d6eb7325d7bc7e67569f3e9b2b316
Ramdan <dadanisme@upi.edu>
2025-08-22 17:34:58 +0800

    mantap accessibility nya jalan

--- Gradeva/Views/Home/Components/SummaryView.swift
+++ Gradeva/Views/Home/Components/SummaryView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct SummaryView: View {
     @ObservedObject private var subjectsManager = SubjectsManager.shared
     @ObservedObject private var batchManager = BatchManager.shared
+    @Environment(\.dynamicTypeSize) private var dynamicTypeSize
+    
+    private var isTextLarge: Bool {
+        dynamicTypeSize > .xxLarge
+    }
     
     private var pendingReview: Int {
         subjectsManager.selectedSubject?.pendingReview ?? 0
@@ -44,21 +49,26 @@ struct SummaryView: View {
             
             VStack(spacing: 32) {
                 ZStack {
-                    // Background circle
-                    Circle()
-                        .stroke(lineWidth: 12)
-                        .foregroundColor(.white)
-                        .accessibilityHidden(true)
-                    
-                    // Progress circle
-                    Circle()
-                        .trim(from: 0.0, to: CGFloat(progress))
-                        .stroke(
-                            Color.appPrimaryDarker,
-                            style: StrokeStyle(lineWidth: 12, lineCap: .round, lineJoin: .round)
-                        )
-                        .rotationEffect(.degrees(-90))
-                        .accessibilityHidden(true)
+                    if !isTextLarge {
+                        ZStack {
+                            // Background circle
+                            Circle()
+                                .stroke(lineWidth: 12)
+                                .foregroundColor(.white)
+                                .accessibilityHidden(true)
+                            
+                            // Progress circle
+                            Circle()
+                                .trim(from: 0.0, to: CGFloat(progress))
+                                .stroke(
+                                    Color.appPrimaryDarker,
+                                    style: StrokeStyle(lineWidth: 12, lineCap: .round, lineJoin: .round)
+                                )
+                                .rotationEffect(.degrees(-90))
+                                .accessibilityHidden(true)
+                        }
+                        .frame(width: 150, height: 150)
+                    }
                     
                     // Text in center
                     VStack {
@@ -79,15 +89,16 @@ struct SummaryView: View {
                             .opacity(0.7)
                             .accessibilityHidden(true)
                     }
+                    .frame(maxWidth: .infinity)
                     .foregroundColor(.white)
                 }
-                .frame(width: 150, height: 150)
+                .frame(maxWidth: .infinity)
                 .accessibilityElement(children: .combine)
                 .accessibilityLabel("Grading progress")
                 .accessibilityValue("\(totalStudents - pendingReview) out of \(totalStudents) assignments graded")
                 .accessibilityAddTraits(.isStaticText)
                 
-                HStack(spacing: 12) {
+                DynamicHStack(spacing: 12) {
                     VStack(alignment: .leading) {
                         HStack(alignment: .top) {
                             Text(studentsPassed.description)
@@ -98,6 +109,7 @@ struct SummaryView: View {
                                 .font(.title)
                                 .accessibilityHidden(true)
                         }
+                        Spacer()
                         Text("Passed")
                             .font(.headline)
                             .fontWeight(.medium)
@@ -105,7 +117,7 @@ struct SummaryView: View {
                     }
                     .padding()
                     .frame(maxWidth: .infinity)
-                    .frame(height: 80)
+                    .frame(maxHeight: .infinity)
                     .background(.appSuccess)
                     .clipShape(RoundedRectangle(cornerRadius: 12))
                     .accessibilityElement(children: .combine)
@@ -122,6 +134,7 @@ struct SummaryView: View {
                                 .font(.title)
                                 .accessibilityHidden(true)
                         }
+                        Spacer()
                         Text("Need assistance")
                             .font(.headline)
                             .fontWeight(.medium)
@@ -129,7 +142,7 @@ struct SummaryView: View {
                     }
                     .padding()
                     .frame(maxWidth: .infinity)
-                    .frame(height: 80)
+                    .frame(maxHeight: .infinity)
                     .background(.appDestructive)
                     .clipShape(RoundedRectangle(cornerRadius: 12))
                     .accessibilityElement(children: .combine)
